4ad5b9f4c97dc6386719be9951111d30e0d79acc
Sean Gorsky <gorsky@Seans-MacBook-Pro.local>
2022-08-18 21:11:14 -0400

    Added test

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -64,3 +64,13 @@ def test_simple_emr_log():
     assert (
         parsed["metadata"]["application_info"]["name"] == "Text Similarity"
     ), "Name is as expected"
+
+
+def test_emr_missing_sql_events():
+    event_log_path = Path("tests", "logs", "emr_missing_sql_events.zip").resolve()
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        event_log = eventlog.EventLogBuilder(event_log_path.as_uri(), temp_dir).build()
+        obj = sparkApplication(eventlog=str(event_log))
+
+    assert list(obj.sqlData.index.values) == [0, 2, 3, 5, 6, 7, 8]
