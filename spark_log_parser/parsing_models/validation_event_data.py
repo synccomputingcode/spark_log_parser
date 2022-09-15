62442ce730f6c0e6f01dc4c0e399626539db09a5
Scott Romney <scott@synccomputing.com>
2022-09-15 01:32:05 -0400

    [PROD-429] Handle event logs with no application end events

--- spark_log_parser/parsing_models/validation_event_data.py
+++ spark_log_parser/parsing_models/validation_event_data.py
@@ -1,19 +1,21 @@
+import logging
+
 from .exceptions import LazyEventValidationException
 from .exceptions import ParserErrorMessages as MSGS
 
-import logging
-
 logger = logging.getLogger("EventDataValidation")
 
-class EventDataValidation():
+
+class EventDataValidation:
     """
     Validate the existence of certain Spark Listener Events
     """
+
     def __init__(self, app=None, debug=False):
 
         self.app = app
-        self.debug = debug # When 'True' disables exception raises for debugging
-        self.message = ''
+        self.debug = debug  # When 'True' disables exception raises for debugging
+        self.message = ""
 
     def validate(self):
         """
@@ -21,44 +23,48 @@ class EventDataValidation():
         and exception. Logging is used here so that the problem is still indicated when in debug
         mode.
         """
+        if not self.app.finish_time:
+            self.message += (
+                f"{MSGS.MISSING_EVENT_GENERIC_MESSAGE} 'Application / Stage / SQL Completion'. "
+            )
+
         self.validate_job_events()
         self.validate_stage_events()
 
-        if (len(self.message)>0):
+        if len(self.message) > 0:
             logger.error(self.message)
             if not self.debug:
-                raise LazyEventValidationException(error_message = self.message)
+                raise LazyEventValidationException(error_message=self.message)
 
     def validate_job_events(self):
         """
-        Look for missing job events. 
-        
+        Look for missing job events.
+
         4/20/2022: Currently only the JobComplete is detected, because a missing
         JobStart event will result in a more urgent exception being thrown in SparkApplication
         """
 
         miss_job_ends = []
         for jid, job in self.app.jobs.items():
-            if not hasattr(job, 'completion_time'):
+            if not hasattr(job, "completion_time"):
                 miss_job_ends.append(jid)
 
-        if len(miss_job_ends)>0:
-            msg = f'{MSGS.MISSING_EVENT_JOB_END}{miss_job_ends}. '
-            self.message += f'{MSGS.MISSING_EVENT_JOB_END}{miss_job_ends}. '
+        if len(miss_job_ends) > 0:
+            self.message += f"{MSGS.MISSING_EVENT_JOB_END}{miss_job_ends}. "
 
     def validate_stage_events(self):
         """
-        Look for missing stage events. 
-        
-        4/20/2022: Currently only the StageSubmitted is detected, because a 
-        missing StageCompleted event will result in a more urgent exception being thrown in 
+        Look for missing stage events.
+
+        4/20/2022: Currently only the StageSubmitted is detected, because a
+        missing StageCompleted event will result in a more urgent exception being thrown in
         SparkApplication
         """
         miss_stage_completes = []
         for jid, job in self.app.jobs.items():
             for sid, stage in job.stages.items():
-                if not hasattr(stage, 'completion_time'):
+                if not hasattr(stage, "completion_time"):
                     miss_stage_completes.append(sid)
 
-        if len(miss_stage_completes)>0:
-            self.message += f'{MSGS.MISSING_EVENT_STAGE_COMPLETE}{miss_stage_completes}.  '
+        if len(miss_stage_completes) > 0:
+            self.message += f"{MSGS.MISSING_EVENT_STAGE_COMPLETE}{miss_stage_completes}.  "
