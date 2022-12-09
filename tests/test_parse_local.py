cac187ae1f94eda818511f5da8633f60e833e827
gseyffert-sync <graham@synccomputing.com>
2022-12-09 11:05:31 -0800

    Implement concrete classes for local file parsing, add unit tests. Include fixtures for automatically testing .tgz/.tar.gz archives given some .zip archive.

---
+++ tests/test_parse_local.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+
+import pytest
+
+from spark_log_parser.parsing_models.application_model_v2 import create_spark_application
+from tests import parsed_files, assert_all_files_identical, PARSED_KEYS
+
+
+def get_spark_app_from_raw_log(event_log_path):
+    return create_spark_application(spark_eventlog_path=str(event_log_path))
+
+
+def get_parsed_log(event_log_path):
+    return get_spark_app_from_raw_log(event_log_path).to_dict()
+
+
+def test_simple_databricks_log():
+    path = Path("tests", "logs", "databricks.json").resolve()
+    parsed_app = get_parsed_log(path)
+    assert all(key in parsed_app for key in PARSED_KEYS), "Not all keys are present"
+    assert (
+        parsed_app["metadata"]["application_info"]["name"] == "Databricks Shell"
+    ), "Name is as expected"
+
+
+@pytest.mark.parametrize("parsed_files",
+                         [(Path("tests", "logs", "databricks.zip").resolve(), get_parsed_log)],
+                         indirect=["parsed_files"])
+def test_simple_databricks_archive(parsed_files):
+    for file in parsed_files:
+        assert all(key in file for key in PARSED_KEYS), "Not all keys are present"
+        assert (
+            file["metadata"]["application_info"]["name"] == "Databricks Shell"
+        ), "Name is as expected"
+
+    assert all(file == parsed_files[0] for file in
+               parsed_files[1:]), "Expected all parsed files to be the same, but they were not"
+
+
+@pytest.mark.parametrize("parsed_files",
+                         [(Path("tests", "logs", "emr.zip").resolve(), get_parsed_log)],
+                         indirect=["parsed_files"])
+def test_simple_emr_log(parsed_files):
+    for file in parsed_files:
+        assert all(key in file for key in PARSED_KEYS), "Not all keys are present"
+        assert (
+            file["metadata"]["application_info"]["name"] == "Text Similarity"
+        ), "Name is as expected"
+
+    assert all(file == parsed_files[0] for file in
+               parsed_files[1:]), "Expected all parsed files to be the same, but they were not"
+
+
+@pytest.mark.parametrize("parsed_files",
+                         [(Path("tests", "logs", "emr_missing_sql_events.zip").resolve(), get_spark_app_from_raw_log)],
+                         indirect=["parsed_files"])
+def test_emr_missing_sql_events(parsed_files):
+    assert_all_files_identical([spark_app.to_dict() for spark_app in parsed_files])
+
+    for spark_app in parsed_files:
+        sql_data = spark_app.sqlData
+        assert sql_data.index.name == "sql_id"
+        assert list(sql_data.index.values) == [0, 2, 3, 5, 6, 7, 8]
+
+
+@pytest.mark.parametrize("parsed_files",
+                         [(Path("tests", "logs", "databricks-rollover-messy.zip").resolve(), get_parsed_log)],
+                         indirect=["parsed_files"])
+def test_databricks_rollover(parsed_files):
+    assert_all_files_identical(parsed_files)
+
+
+def test_parsed_log():
+    """
+    Test that re-hydrating a parsed spark application contains all the keys we would expect it to
+    """
+    event_log_path = Path("tests", "logs", "similarity_parsed.json.gz").resolve()
+    rehydrated = create_spark_application(spark_eventlog_parsed_path=str(event_log_path)).to_dict()
+    assert all(key in rehydrated for key in PARSED_KEYS), "Not all keys are present in re-hydrated Spark application"
